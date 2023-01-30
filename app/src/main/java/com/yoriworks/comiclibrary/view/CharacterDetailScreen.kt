eea1b6c0e4aee6896d14369b502767a1f456265f
Yohanes Rizky Gumilir <yohanes.rizky@ai.astra.co.id>
2023-01-30 11:45:56 +0700

    add navigation section

---
+++ app/src/main/java/com/yoriworks/comiclibrary/view/CharacterDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.yoriworks.comiclibrary.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CharacterDetailScreen() {
+    Text(text = "Character Detail Screen")
+}
